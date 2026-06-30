1ceb02fa4359b5652324725af5b4be4a74c2d8bb
SukkaW <isukkaw@gmail.com>
2026-06-30 11:25:49 +0800

    refactor: drop `ban-useless-function` for now

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,6 @@ import reactPreferFoxactUseAbortableEffect from './rules/react-prefer-foxact-use
 import preferFoxtsErrorUtil from './rules/prefer-foxts-error-util';
 import preferFoxtsArrayUtils from './rules/prefer-foxts-array-utils';
 import avoidStringStartsWithSingleChar from './rules/avoid-string-starts-with-single-char';
-import banUselessFunction from './rules/ban-useless-function';
 import reactNoUseStateAsRef from './rules/react-no-use-state-as-ref';
 import reactNoPerformanceImpactingArrayFind from './rules/react-no-performance-impacting-array-find';
 import noRegexInFunction from './rules/no-regex-in-function';
@@ -112,7 +111,6 @@ const plugin = {
         'sukka/prefer-foxts-error-util': 'error',
         'sukka/prefer-foxts-array-utils': 'error',
         'sukka/avoid-string-starts-with-single-char': 'error',
-        'sukka/ban-useless-function': 'warn',
         'sukka/no-regex-in-function': 'warn',
         'sukka/no-constant-array-includes': 'warn',
         'sukka/track-todo-fixme-comment': 'warn'
@@ -226,7 +224,6 @@ const plugin = {
     'prefer-foxts-error-util': preferFoxtsErrorUtil,
     'prefer-foxts-array-utils': preferFoxtsArrayUtils,
     'avoid-string-starts-with-single-char': avoidStringStartsWithSingleChar,
-    'ban-useless-function': banUselessFunction,
     'react-no-use-state-as-ref': reactNoUseStateAsRef,
     'react-no-performance-impacting-array-find': reactNoPerformanceImpactingArrayFind,
     'no-regex-in-function': noRegexInFunction,
